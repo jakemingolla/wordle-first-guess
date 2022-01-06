ad228c4c952447acbf05044184496e49031b6c60
Jake Mingolla <jakemingolla@gmail.com>
2022-01-06 16:25:37 +0000

    add script

--- README.md
+++ README.md
@@ -1,1 +1,92 @@
 # wordle-first-guess
+
+Script to exhaustively determine the best guess for
+[Wordle](https://www.powerlanguage.co.uk/wordle/).
+
+## TLDR
+_Note:_ A word's "score" is how many words share the same letters at
+each of its letter's positions.
+
+```
+Top 5 words:
+┌─────────┬─────────┬───────┐
+│ (index) │  word   │ score │
+├─────────┼─────────┼───────┤
+│    0    │ 'sanes' │ 15085 │
+│    1    │ 'sales' │ 14931 │
+│    2    │ 'bares' │ 14878 │
+│    3    │ 'cares' │ 14835 │
+│    4    │ 'tares' │ 14587 │
+└─────────┴─────────┴───────┘
+
+Top 5 words (no upper case or duplicate letters):
+┌─────────┬─────────┬───────┐
+│ (index) │  word   │ score │
+├─────────┼─────────┼───────┤
+│    0    │ 'bares' │ 14878 │
+│    1    │ 'cares' │ 14835 │
+│    2    │ 'tares' │ 14587 │
+│    3    │ 'mares' │ 14548 │
+│    4    │ 'pares' │ 14507 │
+└─────────┴─────────┴───────┘
+
+Top 5 words (letter position agnostic):
+┌─────────┬─────────┬───────┐
+│ (index) │  word   │ score │
+├─────────┼─────────┼───────┤
+│    0    │ 'Aeaea' │ 18813 │
+│    1    │ 'sessa' │ 18656 │
+│    2    │ 'sasse' │ 18656 │
+│    3    │ 'asses' │ 18656 │
+│    4    │ 'eases' │ 18141 │
+└─────────┴─────────┴───────┘
+
+Top 5 words (letter position agnostic, no upper case or duplicate letters):
+┌─────────┬─────────┬───────┐
+│ (index) │  word   │ score │
+├─────────┼─────────┼───────┤
+│    0    │ 'seora' │ 16392 │
+│    1    │ 'arose' │ 16392 │
+│    2    │ 'aeons' │ 15948 │
+│    3    │ 'osela' │ 15794 │
+│    4    │ 'alose' │ 15794 │
+└─────────┴─────────┴───────┘
+```
+
+## Usage
+```
+nvm use
+node index.js
+```
+
+_Note:_ Replace `words.txt` with a more up-to-date dictionary of all
+words (newline delimited) if desired.
+
+## Algorithm Used
+
+1. Add each word that is exactly 5 characters long to a case-insensitive
+   dictionary.
+
+2. For each letter in all valid words, add 1 'point' to a series of parallel
+   dictionaries corresponding to the positional index of the letter in the word.
+
+3. Score each valid word by adding up the occurences of each of its letters
+   based on the positional letter dictionary at the corresponding index.
+
+```
+Example Word: apple
+Positional Letter Dictionaries:
+[
+  {     a: 1, b: 0, c: 0, ... },
+  { ... o: 0, p: 1, q: 0, ... },
+  { ... o: 0, p: 1, q: 0, ... },
+  { ... k: 0, l: 1, m: 0, ... },
+  { ... d: 0, e: 1, f: 0, ... }
+]
+```
+
+## Caveats
+- It is difficult to determine the 'cost' of duplicate letters in a guess
+- Some words in the dictionary are more likely to be the Wordle than others
+- Some of the words in the dictionary are pretty much nonsense
+  (see "Aeaea" above)
---
+++ index.js
@@ -0,0 +1,99 @@
+const fs = require('fs').promises;
+const FILE_NAME = 'words.txt';
+const ALL_LETTERS_REGEX = /^[a-zA-Z]+$/;
+const UPPER_CASE_LETTERS_REGEX = /[A-Z]+/;
+const ALPHA = Array.from(Array(26)).map((e, i) => i + 97);
+const ALPHABET = ALPHA.map((x) => String.fromCharCode(x));
+const NUM_LETTERS_IN_WORD = 5;
+const NUM_WORDS_REPORTED = 5;
+
+const generateLetterDictionary = () => {
+  return ALPHABET.reduce((accumulator, letter) => {
+    accumulator[letter] = 0;
+    return accumulator;
+  }, {});
+};
+
+const containsDuplicateLetters = word => {
+  const letters = word.split('');
+  return new Set(letters).size !== letters.length;
+};
+
+const main = async () => {
+  const raw = await fs.readFile(FILE_NAME, { encoding: 'utf-8' });
+  const allWords = raw.split('\n');
+  const validWords = allWords.filter((word) => {
+    return word.length === NUM_LETTERS_IN_WORD && ALL_LETTERS_REGEX.test(word);
+  });
+
+  console.log(`There are ${allWords.length} words, ` +
+    `${validWords.length} of which are ${NUM_LETTERS_IN_WORD} letters.`);
+
+  const wordDictionary = {};
+  const positionalLetterDictionary = [
+    generateLetterDictionary(),
+    generateLetterDictionary(),
+    generateLetterDictionary(),
+    generateLetterDictionary(),
+    generateLetterDictionary()
+  ];
+
+  validWords.forEach((word) => {
+    wordDictionary[word] = true;
+    word.split('').forEach((letter, index) => {
+      letter = letter.toLowerCase();
+      positionalLetterDictionary[index][letter] += 1;
+    });
+  });
+
+  const scoredWords = validWords.map((word) => {
+    const scores = word.split('').map((letter, index) => {
+      letter = letter.toLowerCase();
+      return positionalLetterDictionary[index][letter];
+    });
+
+    return { word, score: scores.reduce((a, b) => a + b, 0) };
+  }).sort((a, b) => a.score < b.score ? 1 : -1);
+
+  const scoredWordsPositionAgnostic = validWords.map((word) => {
+    const scores = word.split('').map((letter) => {
+      letter = letter.toLowerCase();
+      return Math.max(
+        ...positionalLetterDictionary.map((x) => x[letter])
+      );
+    });
+
+    return { word, score: scores.reduce((a, b) => a + b, 0) };
+  }).sort((a, b) => a.score < b.score ? 1 : -1);
+
+  const topWords = scoredWords.slice(0, NUM_WORDS_REPORTED);
+  const topWordsNoUpperCaseOrDuplicates = scoredWords
+    .filter((x) => {
+      return !UPPER_CASE_LETTERS_REGEX.test(x.word)  &&
+        !containsDuplicateLetters(x.word);
+    })
+    .slice(0, NUM_WORDS_REPORTED);
+  const topWordsPositionAgnostic = scoredWordsPositionAgnostic
+    .slice(0, NUM_WORDS_REPORTED);
+  const topWordsPositionAgnosticNoUpperCaseOrDuplicates =
+    scoredWordsPositionAgnostic
+      .filter((x) => {
+        return !UPPER_CASE_LETTERS_REGEX.test(x.word)  &&
+          !containsDuplicateLetters(x.word);
+      })
+      .slice(0, NUM_WORDS_REPORTED);
+    
+
+  console.log(`\nTop ${NUM_WORDS_REPORTED} words:`);
+  console.table(topWords);
+  console.log(`\nTop ${NUM_WORDS_REPORTED} words ` +
+    '(no upper case or duplicate letters):');
+  console.table(topWordsNoUpperCaseOrDuplicates);
+  console.log(`\nTop ${NUM_WORDS_REPORTED} words (letter position agnostic):`);
+  console.table(topWordsPositionAgnostic);
+  console.log(`\nTop ${NUM_WORDS_REPORTED} words ` +
+    '(letter position agnostic, no upper case or duplicate letters):');
+  console.table(topWordsPositionAgnosticNoUpperCaseOrDuplicates);
+};
+
+return main();
